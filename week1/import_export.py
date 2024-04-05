e72d1183004fcada0cb1400eccb372160a03c8bd
Amshu G M <99896640+AmshuGM@users.noreply.github.com>
2024-04-05 14:47:41 +0530

    Create import_export.py

---
+++ week1/import_export.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+airbnb_data = pd.read_csv("dataset.csv")
+airbnb_data.head()
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# Define the column names
+
+col_names = ["sepal_length_in_cm",
+
+"sepal_width_in_cm",
+
+"petal_length_in_cm",
+
+"petal_width_in_cm",
+
+"class"]
+
+# Read data from URL
+
+iris_data = pd.read_csv(url, names=col_names)
+
+iris_data.to_csv("cleaned_iris_data.csv")
